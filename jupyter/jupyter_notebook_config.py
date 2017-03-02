268d05ca4651330eab5c25553054d14d867cac47
Liuyang-Z <zlyang65@gmail.com>
2017-03-03 00:07:39 +0800

    modify the config file

--- jupyter/jupyter_notebook_config.py
+++ jupyter/jupyter_notebook_config.py
@@ -249,7 +249,7 @@ c.NotebookApp.port = 8888
 ## Whether to trust or not X-Scheme/X-Forwarded-Proto and X-Real-Ip/X-Forwarded-
 #  For headerssent by the upstream reverse proxy. Necessary if the proxy handles
 #  SSL
-#c.NotebookApp.trust_xheaders = False
+c.NotebookApp.trust_xheaders = True
 
 ## DEPRECATED, use tornado_settings
 #c.NotebookApp.webapp_settings = {}
