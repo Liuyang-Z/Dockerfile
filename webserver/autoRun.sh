05f8f475f134c6f9cea009203e58b48045a7f95a
liuyang-z <liuyang.z@hooowl.com>
2019-08-11 21:33:41 +0800

    增加对 nginx tls1.3 的支持，去除 php

--- webserver/autoRun.sh
+++ webserver/autoRun.sh
@@ -2,14 +2,8 @@
 
 mkdir -p /data/nginx/conf.d/
 mkdir -p /data/nginx/ssl/
-mkdir -p /data/php/
 mkdir -p /data/html/
 
-if ! [ -f /data/php/php.ini ]; then
-    cp /usr/local/php/etc/php.ini /data/php/php.ini
-else
-    cp -f /data/php/php.ini /usr/local/php/etc/php.ini
-fi
 
 if ! [ -f /data/nginx/nginx.conf ]; then
     cp /usr/local/nginx/conf/nginx.conf /data/nginx/nginx.conf
@@ -21,7 +15,6 @@ if ! [ -f /data/nginx/conf.d/default.conf ]; then
     cp /usr/local/nginx/conf/conf.d/default.conf /data/nginx/conf.d/default.conf
 fi
 
-/usr/local/php/sbin/php-fpm
 /usr/local/nginx/sbin/nginx
 
 /bin/bash
